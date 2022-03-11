0c47612f5a358d01775c4117eb4b866179309c68
Luis Ángel Vega Rodríguez <vrl4@outlook.es>
2022-03-11 09:40:52 -0600

    feat: linking routes with db SP

--- logic-layer/src/controllers/controller.js
+++ logic-layer/src/controllers/controller.js
@@ -2,7 +2,6 @@ import { request } from 'express';
 import { Int, pool } from 'mssql';
 import { send } from 'process';
 import { getConnection } from '../database/connection'
-const sp_connection = require('../database/sp_connection')
 const path = require('path')
 
 export const invalidRoute = async (req, res) => {
@@ -10,13 +9,21 @@ export const invalidRoute = async (req, res) => {
 };
 
 export const showLogin = async (req, res) => {
-    const existUser = sp_connection.checkUser()
     res.send('Showing login page');
    //res.sendFile(path.join(__dirname, '../../../interface-layer/src/index.html'));
 }
 
 export const login = async (req, res) => {
+    const pool = await getConnection();
+    const result = await pool.request()
+        .input('inUserName', 'Juan')
+        .input('inPassword', 'j123')
+        .output('OutStatus', 0)
+        .output('OutResult', 0)
+        .execute('CheckUser');
+    console.log(result);
     res.send('Sending data for login')
+    return result;
 };
 
 export const getAllArticles = async (req, res) => {
@@ -24,18 +31,40 @@ export const getAllArticles = async (req, res) => {
 };
 
 export const consultByPattern  = async (req, res) => {
+    const pool = await getConnection();
+    const result = await pool.request()
+        .input('inName', 'a')
+        .output('outResult', 0)
+        .execute('FilterByName');
+    console.log(result);
     res.send("Showing pattern search result")
+   // return result;
 };
 
 export const consultByAmount = async (req, res) => {
+    const pool = await getConnection();
+    const result = await pool.request()
+        .input('inAmount', 10)
+        .output('outResult', 0)
+        .execute('FilterTopAmount');
+    console.log(result);
+
     res.send("Showing amount search result")
 };
 
 export const showInsert = async (req, res) => {
+    
     res.send("Showing insert page")
 };
 
 export const insert = async (req, res) => {
+    const pool = await getConnection();
+    const result = await pool.request()
+        .input('inName', 'new')
+        .input('inPrice', 100)
+        .output('outResult', 0)
+        .execute('InsertArticle');
+    console.log(result);
     res.send("Sending insert data")
 };
 export const getUserCredentials = async (req, res) => {
