d383efc7f47d2f83d23bc84732f7db8e4f142718
Luis Ángel Vega Rodríguez <vrl4@outlook.es>
2022-03-21 19:37:12 -0600

    style: removin unused lines

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,6 @@ const config = process.env;
 
 export const verifyToken = (req, res, next) => {
     const authHeader = req.headers.authorization;
-    console.log(authHeader + "  auth header");
     if (typeof authHeader !== 'undefined') {
         jwt.verify(authHeader, config.TOKEN_KEY, (error, token) => {
             req.token = token;
